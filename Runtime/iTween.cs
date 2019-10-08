cf61e349beae88dbe4031fa161c560b8abf093a1
Route 1 Rodent <18262122+route1rodent@users.noreply.github.com>
2019-10-08 22:30:55 +0200

    fix iTween.cs version as reflected in unity Asset Store

--- Runtime/iTween.cs
+++ Runtime/iTween.cs
@@ -40,7 +40,7 @@ using UnityEngine;
 #endregion
 
 /// <summary>
-/// <para>Version: 2.0.8</para>	 
+/// <para>Version: 2.0.10</para>
 /// <para>Author: Bob Berkebile (http://pixelplacement.com)</para>
 /// <para>Support: http://itween.pixelplacement.com</para>
 /// </summary>
